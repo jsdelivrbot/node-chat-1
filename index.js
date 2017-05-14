fdcdadcf01dcae3144e943efad0f03fdb7f1ec12
Piotr <pituzz.01@gmail.com>
2017-05-14 18:27:13 +0200

    ff

--- index.js
+++ index.js
@@ -54,6 +54,6 @@ io.on('connection', function (client) {
       io.emit('chat message', userName + ":  " + msg);
   });
 });
-http.listen(3000, function () {
+http.listen(80, function () {
   console.log('listening on *:3000');
 });
